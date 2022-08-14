ed7ddc1a820c74c0d6397c2fc299a07071998c25
p5eudoDjnn <alexander.havers@gmail.com>
2022-08-14 17:34:59 -0400

    update fetch request to discern string of api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-let getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
-  console.log("Calling getUserRepos");
+let getUserRepos = function (user) {
+  // format the github api url to call "user" args
+  let apiURL = "https://api.github.com/users/" + user + "/repos";
+
+  // Call request to url using fetch
+  fetch(apiURL).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
+
 getUserRepos();
